e5ca6814fde9718513e6b4b28a188a8551c86388
Adam Mocarski <mocarski.praca@gmail.com>
2017-07-02 16:53:18 +0100

    test

--- MainProject/MainProject/Controllers/HomeController.cs
+++ MainProject/MainProject/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace MainProject.Controllers
         public IActionResult Index()
         {
             //nothing branch2
+            // second test
             return View();
         }
 
